2ad4b00c481a11b3379f00e17c4d0abb170ed42d
Zatelbeeit <breaul@gmail.com>
2019-09-09 15:53:15 +0200

    exo 4 updated

--- script4.js
+++ script4.js
@@ -1,7 +1,11 @@
-var prenom = "";
-var nom = "Lou";
-var ville = "Mende";
+var nom = document.getElementById("nom").value;
+var prenom = document.getElementById("prenom").value;
+var ville = document.getElementById("ville").value;
+
 var valider = document.getElementById("valider");
-valider.addEventListener("click", function(){
-    alert("Bonjour \n" + prenom + "\n" + nom +"\n" + ville);
-})
+//valider.addEventListener("click", function(){
+
+function valider (){
+alert("Bonjour \n" + getElementById("prenom").value + "\n" + nom +"\n" + ville);
+}
+//})
