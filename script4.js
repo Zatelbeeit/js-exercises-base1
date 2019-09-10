e321541c255e44e0f9960fbeb42b0cdbf50ae498
Zatelbeeit <breaul@gmail.com>
2019-09-10 09:58:19 +0200

    updates

--- script4.js
+++ script4.js
@@ -3,9 +3,9 @@ var prenom = document.getElementById("prenom").value;
 var ville = document.getElementById("ville").value;
 
 var valider = document.getElementById("valider");
-//valider.addEventListener("click", function(){
+valider.addEventListener("click", function(){
 
-function valider (){
-alert("Bonjour \n" + getElementById("prenom").value + "\n" + nom +"\n" + ville);
-}
-//})
+
+alert("Bonjour \n" + getElementById("prenom").value + "\n" + nom +"\n" + ville)
+
+})
